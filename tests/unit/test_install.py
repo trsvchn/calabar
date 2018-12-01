d99493a4325799172aab4b352d923b1572f8582f
Taras Savchyn <savchyntm@gmail.com>
2018-12-01 22:26:42 +0200

    Small changes

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -1,28 +1,30 @@
+from os.path import exists
 import pytest
 from conftest import *
 
 
-def test_default_install(default_install):
-    assert default_install.accelerator == 'cpu'
-    assert default_install.platform is None
-    assert default_install.cuda_version is None
-    assert default_install.nvidia_device == NVIDIA_PATH
+def test_default_install(install):
+    assert install.accelerator == 'cpu'
+    assert install.platform is None
+    assert install.cuda_version is None
+    assert install.nvidia_device == NVIDIA_PATH
 
 
-def test_set_gpu_accelerator(default_install):
-    default_install._set_gpu_accelerator()
-    assert default_install.cuda_version == CUDA_VERSION
-    assert default_install.accelerator == GPU_ACCELERATOR
+@pytest.mark.skipif(not exists(NVIDIA_PATH), reason='Requires CUDA')
+def test_set_gpu_accelerator(install):
+    install._set_gpu_accelerator()
+    assert install.cuda_version == CUDA_VERSION
+    assert install.accelerator == GPU_ACCELERATOR
 
 
-def test_pip_install_valid(default_install):
-    assert default_install._pip_install(TEST_PACKAGE_VALID) == 0
+def test_pip_install_valid(install):
+    assert install._pip_install(TEST_PACKAGE_VALID) == 0
 
 
-def test_pip_install_invalid_input_type(default_install):
+def test_pip_install_invalid_input_type(install):
     with pytest.raises(AssertionError):
-        default_install._pip_install(TEST_PACKAGE_INVALID_TYPE)
+        install._pip_install(TEST_PACKAGE_INVALID_TYPE)
 
 
-def test_pip_install_invalid_input_package(default_install):
-    assert default_install._pip_install(TEST_PACKAGE_INVALID) is None
+def test_pip_install_invalid_input_package(install):
+    assert install._pip_install(TEST_PACKAGE_INVALID) is None
