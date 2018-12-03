b54f3e08d1e28fb9eadf3088c01f967986d59c45
Taras Savchyn <savchyntm@gmail.com>
2018-12-03 23:11:20 +0200

    Implemented save/upload to drive feature

--- easycolab/driveutils.py
+++ easycolab/driveutils.py
@@ -7,13 +7,19 @@ import shutil
 import errno
 from google.colab import drive
 
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
 
 class MountCopy:
-    """Mount your Google drive and copy folder/files"""
+    """Use this class to mount your Google Drive and copy folder/files"""
 
     def __init__(self, mounting_point: str = '/drive'):
         """
         :param mounting_point: destination where to mount drive
+
         Default: '/drive'
         """
         self.mounting_point = mounting_point
@@ -21,16 +27,17 @@ class MountCopy:
     def mount_drive(self) -> None:
         """
         Mounts Drive to specified location.
+
         :return: None
         """
         drive.mount(self.mounting_point)
         print(f'Google drive mounted on {self.mounting_point}')
         self.mounting_point = os.path.join(self.mounting_point, 'My Drive')
-        print(self.mounting_point)
 
     def copy_from_drive(self, source: str, dest: str) -> None:
         """
         Copies file or folder from mounted folder.
+
         :param source: str, file or folder from drive to be copied. Ex: data.tar.gz
         You don't need to specify the full path /drive/My\ Drive/data.tar.gz, just point file/folder
         from your mounting point
@@ -54,6 +61,7 @@ class MountCopy:
     def __call__(self, source: str, dest: str) -> None:
         """
         Mounts and copies file/folder in one line
+
         :param mounting_point:
         :param source: str, file or folder from drive to be copied. Ex: data.tar.gz
         :param dest: str, destination path
@@ -61,3 +69,38 @@ class MountCopy:
         """
         self.mount_drive()
         self.copy_from_drive(source, dest)
+
+
+class SaveToDrive:
+    """
+    Provides authorization to Google Drive and uploads file to it.
+    """
+
+    def __init__(self):
+        """
+        Authorization Step
+        """
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+        self.drive = GoogleDrive(gauth)
+
+    def save2drive(self, file: str) -> None:
+        """
+        Save file from Colab instance directly to Google Drive
+
+        :param file: str, file to upload
+        :return: None
+        """
+        file_name = os.path.basename(file)
+        print(f'Uploading {file_name}...')
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+        self.drive = GoogleDrive(gauth)
+
+        uploaded = self.drive.CreateFile({'title': f'{file_name}'})
+        uploaded.SetContentFile(f'{file}')
+        uploaded.Upload()
+        new_file_id = uploaded.get('id')
+        print(f'{file_name} Successfully Uploaded. File ID: {new_file_id}')
