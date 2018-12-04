48bdc189b8e6685429415a424bbe590f6fc01a90
Taras Savchyn <savchyntm@gmail.com>
2018-12-05 00:57:36 +0200

    Add unit tests for utils

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,29 @@
+from os.path import exists
+import pytest
+from conftest import *
+from easycolab import utils
+
+
+@pytest.mark.skipif(not exists(NVIDIA_PATH), reason='Requires NVIDIA driver')
+def test_get_gpu_usage_output():
+    test_out = utils.get_gpu_usage()
+    assert test_out
+    assert isinstance(test_out, str)
+
+
+def test_get_disk_usage_output():
+    test_out = utils.get_disk_usage()
+    assert test_out
+    assert isinstance(test_out, str)
+
+
+def test_get_distro_descr_output():
+    test_out = utils.get_distro_descr()
+    assert test_out
+    assert isinstance(test_out, str)
+
+
+def test_current_time_output():
+    test_out = utils.current_time()
+    assert test_out
+    assert isinstance(test_out, str)
