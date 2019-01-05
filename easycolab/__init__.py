73c3d8961086096633663863fc4549c0ebc4f4eb
Taras Savchyn <savchyntm@gmail.com>
2019-01-05 19:27:16 +0200

    Fix imports for package

--- easycolab/__init__.py
+++ easycolab/__init__.py
@@ -1,4 +1,6 @@
 from .install import InstallPyTorch
-
+from .driveutils import MyDrive, SaveToDrive
+from .arch import unzip, untar
+from .notifications import Email
 
 install_pytorch = InstallPyTorch()
