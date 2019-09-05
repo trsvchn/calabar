df96715b606adeae617a6ea1ec34d6affd64c104
Taras Savchyn <savchyntm@gmail.com>
2019-09-05 18:29:09 +0300

    Utils refactoring, add some new functions to utils for system info extracting

--- coutils/__init__.py
+++ coutils/__init__.py
@@ -2,3 +2,4 @@ from .install import install_pytorch, upgrade_pytorch
 from .driveutils import MyDrive, SaveToDrive
 from .arch import unzip, untar
 from .notifications import Email
+from .utils import print_sysinfo
