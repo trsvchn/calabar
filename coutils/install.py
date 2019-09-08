9f3a9c35d68b984fb7201fdc9769be0f9a0c5de4
Taras Savchyn <savchyntm@gmail.com>
2019-09-08 14:06:30 +0300

    install module refactoring: remove install pytorch feature, keep pip_install, modify appropriate tests in tests

--- coutils/install.py
+++ coutils/install.py
@@ -1,141 +1,37 @@
 r"""
-Handles installation of libraries, packages etc.
+Handles installation of additional libraries, packages etc.
 """
 
-from os.path import exists
 import subprocess
-from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
 import logging
 
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
-class Install:
-    r"""Base class for installation stuff."""
-
-    def __init__(self):
-        self.accelerator = 'cpu'  # default is CPU
-        self.platform = None
-        self.cuda_version = None
-        self.nvidia_device = '/dev/nvidia0'
-
-    def _set_gpu_accelerator(self) -> None:
-        r"""Set right CUDA version."""
-
-        is_cuda = exists(self.nvidia_device)
-        assert is_cuda, "No GPU detected. Please change runtime type to 'GPU':\
-                         \nRuntime -> Change runtime type -> Hardware accelerator -> GPU"
-
-        cmd = 'ldconfig -p | grep cudart.so'
-        cuda = subprocess.getoutput(cmd).split('.')[2:4]
-        self.cuda_version = ".".join([i[0] for i in cuda])  # setting the cuda version
-        logging.info(f'{self.cuda_version} CUDA version detected!')
-
-        # Set values:
-        cuda = 'cu' + ''.join([i[0] for i in cuda])
-        self.accelerator = cuda  # set accelerator to GPU
-
-    def _set_platform(self) -> None:
-        r"""Set instance platform."""
-
-        self.platform = f'{get_abbr_impl()}{get_impl_ver()}-{get_abi_tag()}'
-        logging.info(f'{self.platform} platform detected!')
-
-    def _pip_install(self, packages: list) -> [int, None]:
-        r"""
-        Install listed packages using pip. Not really useful for external usage.
-
-        Parameters:
-            **packages** (`list`): Packages to install as list of strings.
-        """
-
-        assert isinstance(packages, list), 'input packages is not a list! Please, provide packages as list of str'
-
-        cmd = f'pip install -q {" ".join(packages)}'
-        installation_output = subprocess.call(cmd, shell=True)  # expecting for 0
-
-        if installation_output == 0:
-            logging.info(f'The following packages were installed successfully: {" ".join(packages)}')
-            return installation_output
-        else:
-            logging.critical('Error occurred during installation!')
-            logging.info(f'To get full error message, run this command in the next cell:\n###\n!{cmd}\n###')  # TODO: refactor!!
-            return None
-
-
-class InstallPyTorch(Install):
+def pip_install(packages: str) -> [int, None]:
     r"""
-    .. note::
-        This feature is outdated. Colab already supports with PyTorch out of the box.
+    Install listed packages using pip. Not really useful for external usage.
 
-    Handles PyTorch installation. By default installs PyTorch ``1.0.0`` version compiled with CUDA 9.0.
-    In addition, installation of version ``0.4.1``, and version ``0.4.0`` are also available.
+    Parameters:
+        **packages** (`str`): Packages to install in str format separated by space. Ex: 'numpy scipy'.
     """
 
-    def __init__(self):
-        super(InstallPyTorch, self).__init__()
-        self.link = f'http://download.pytorch.org/whl/'
-        self.nightly_link = 'https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html'
-        self.default_version = '1.0.0'
-        self.install = True
+    if not isinstance(packages, str):
+        raise ValueError('Please, provide packages in a str, Ex: "numpy scipy"')
+    else:
+        packages = packages.strip()
 
-    def _get_user_input(self) -> str:
-        return str(input())
+    cmd = f'pip install -q {packages}'
+    logging.info(f'Installing {packages}...')
+    installation_output = subprocess.call(cmd, shell=True)  # expecting for 0
 
-    def _check_version(self, version: str) -> None:
-        r"""
-        Checks the user specified version. Recommended versions are ``1.0.0`` or ``1.4.1``.
-        """
-
-        if version == '0.4.0':
-            logging.warning(f'Chosen PyTorch version {version} doesn\'t ship with CUDA {self.cuda_version}')
-            logging.warning(f'Recommended PyTorch version for CUDA {self.cuda_version} is {self.default_version}')
-            _ = True
-            while _:
-                logging.warning('Install this version anyway? Type "y" to continue installation or "n" to cancel:')
-                confirm = self._get_user_input()
-                if confirm:
-                    if confirm == 'y':
-                        self.accelerator = 'cu' + '91'
-                        _ = False
-                    elif confirm == 'n':
-                        self.install = False
-                        logging.info('Installation canceled!')
-                        _ = False
-
-    def __call__(self, version: str = None, gpu: bool = True) -> None:
-        r"""
-        Launch Installation process.
-
-        Parameters:
-            **version** (`str`): Version of Pytorch to install. Default: ``1.0.0``
-            **gpu** (`bool`): Install with CUDA support, default: ``True``.
-        """
-
-        logging.info('Launching Installation Process...')
-
-        if not version:
-            version = self.default_version
-        if gpu:
-            self._set_gpu_accelerator()
-            self._check_version(version)
-        if self.install:
-            logging.info(f'{version} PyTorch is going to be installed')
-            if version == '1.0.0':
-                pytorch_latest = ['torch', 'torchvision']
-                logging.info(f'Downloading PyTorch {version}\n...')
-                self._pip_install(pytorch_latest)  # Installing PyTorch and torchvision
-            else:
-                self._set_platform()
-                pytorch_wheel = f'torch-{version}-{self.platform}-linux_x86_64.whl'  # setting PyTorch wheel
-                logging.info(f'Downloading {pytorch_wheel} from {self.link}\n...')
-                pytorch = f'{self.link}{self.accelerator}/{pytorch_wheel}'
-                packages = [pytorch, 'torchvision']
-                self._pip_install(packages)  # Installing PyTorch and torchvision
-
-
-install_pytorch = InstallPyTorch()  # TODO: outdated!!
+    if installation_output == 0:
+        logging.info(f'The following packages were installed successfully: {packages}')
+        return installation_output
+    else:
+        logging.critical('Error occurred during installation!')
+        logging.info(f'To get full error message, run the following command in the next cell:\n###\n!{cmd}\n###')
 
 
 def upgrade_pytorch():
@@ -147,6 +43,7 @@ def upgrade_pytorch():
 
     if installation_output == 0:
         logging.info('torch and torchvision were upgraded to the latest version successfully!')
+        return installation_output
     else:
         logging.critical('Error occurred during installation!')
         logging.info(f'To get full error message, run the following command in the next cell:\n###\n!{cmd}\n###')
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,43 +2,18 @@ import sys
 import pytest
 
 sys.path.append('.')  # for pytest on automatic testing stage
-
 sys.modules['google'] = __import__('mock_import')
 sys.modules['google.colab'] = __import__('mock_import')
 
-from coutils.install import Install, InstallPyTorch, upgrade_pytorch
 
-
-DEFAULT_ACCELERATOR = 'cpu'
-CORRECT_PLATFORM_3 = 'cp36-cp36m'
-CUDA_VERSION = '9.2'
-GPU_ACCELERATOR = 'cu92'
 NVIDIA_PATH = '/dev/nvidia0'
-TEST_PACKAGE_VALID = ['pip', 'pip']
-TEST_PACKAGE_INVALID_TYPE = 'foo'
-TEST_PACKAGE_INVALID = ['foo', 'bar']
 
-DEFAULT_PYTORCH_VERSION = '1.0.0'
-PYTORCH_LINK = 'http://download.pytorch.org/whl/'
+
+# testing installing via pip
+TEST_PACKAGE_VALID = 'pip pip'
+TEST_PACKAGE_INVALID_TYPE = ['foo', 'bar']
+TEST_PACKAGE_INVALID = 'foo bar'
 
 
 FROM_ADDR = 'foo@bar'
 TO_ADDRS = ['foo@foo', 'bar@bar']
-
-
-@pytest.fixture
-def install():
-    """Default installation"""
-    return Install()
-
-
-@pytest.fixture
-def install_pytorch():
-    """Default PyTorch installation"""
-    return InstallPyTorch()
-
-
-@pytest.fixture
-def upgrade():
-    """Upgrading pytorch via pip"""
-    return upgrade_pytorch
